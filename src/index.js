274f33b5331bc1a39fd7cb3440413e7892062046
SegoCode <35817798+SegoCode@users.noreply.github.com>
2021-12-04 17:38:52 +0100

    Update files

--- src/index.js
+++ src/index.js
@@ -9,14 +9,18 @@ const logger = require('./middlewares/logger.conf');
 const router = require('./routes');
 
 const port = process.env.PORT || 8080;
+const processManagerEnabled = false;
+
 const app = express();
 
 if (typeof process.env.NODE_ENV !== 'undefined' && process.env.NODE_ENV.trim() === 'production') {
+  //Hide console logs in production
   console.log = function () {};
-} else {
+} else if (!processManagerEnabled) {
+  //Capture petitions logs out of production without procces manager
   app.use(logger.consoleLogger);
+  app.use(logger.fileLogger);
 }
-app.use(logger.fileLogger);
 
 app.use(compression());
 app.use(bodyParser.json());
@@ -26,14 +30,15 @@ app.use('/api', router); //Apiv1 entry point
 app.use(genericResponses.internalError());
 app.use(genericResponses.notFound());
 
-//Running server entry point
+//Use cluster if you dont have process manager
 let numCPUs = os.cpus().length;
-if (cluster.isMaster) {
+if (cluster.isMaster && !processManagerEnabled) {
   for (var i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 } else {
   app.listen(port, () => {
+    //Launch server entry point
     console.log(`Launch ${process.pid} at http://localhost:${port}`);
   });
 }
