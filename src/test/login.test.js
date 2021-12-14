89a88707fbc40d91e903e4267496a01ef5dd96fa
SegoCode <35817798+SegoCode@users.noreply.github.com>
2021-12-14 12:34:01 +0100

    Update login.test.js

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -4,31 +4,31 @@ const { app, server } = require('./../index');
 api = supertest(app);
 
 beforeEach(() => {
-  //initializeCityDatabase();
+	//initializeCityDatabase();
 });
 
 describe('Testing testing access to endpoints', () => {
-  describe('Public endpoints', () => {
-    test('GET /api/', async () => {
-      await api.get('/api/').expect(200);
-    });
+	describe('Public endpoints', () => {
+		test('GET /api/', async () => {
+			await api.get('/api/').expect(200).expect('Content-Type', /json/);
+		});
 
-    test('POST /api/auth/login/', async () => {
-      let body = {
-        user: 'test',
-        pass: 'test',
-      };
-      await api.post('/api/auth/login/').send(body).expect(200);
-    });
-  });
+		test('POST /api/auth/login/', async () => {
+			let body = {
+				user: 'test',
+				pass: 'test',
+			};
+			await api.post('/api/auth/login/').send(body).expect(200).expect('Content-Type', /json/);
+		});
+	});
 
-  describe('Private endpoints', () => {
-    test('GET /api/auth/login/refresh/', async () => {
-      await api.get('/api/auth/login/refresh/').expect(401);
-    });
-  });
+	describe('Private endpoints', () => {
+		test('GET /api/auth/login/refresh/', async () => {
+			await api.get('/api/auth/login/refresh/').expect(401).expect('Content-Type', /json/);
+		});
+	});
 });
 
 afterAll(() => {
-  server.close();
+	server.close();
 });
