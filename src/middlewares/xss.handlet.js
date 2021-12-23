45cc85e567073fc7a2485d932e24b2d9eae50235
SegoCode <35817798+SegoCode@users.noreply.github.com>
2021-12-23 21:29:54 +0100

    Create xss.handlet.js

---
+++ src/middlewares/xss.handlet.js
@@ -0,0 +1,21 @@
+function clean(data = '') {
+	let isObject = false;
+	if (typeof data === 'object') {
+		data = JSON.stringify(data);
+		isObject = true;
+	}
+
+	data = inHTMLData(data).trim();
+	if (isObject) data = JSON.parse(data);
+
+	return data;
+}
+
+function xssfilter() {
+	return (req, res, next) => {
+		if (req.body) req.body = clean(req.body);
+		if (req.query) req.query = clean(req.query);
+		if (req.params) req.params = clean(req.params);
+		next();
+	};
+}
