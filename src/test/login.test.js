eb3ac6474b7c55bf5d70d68c255a3fb3af97fa3f
SegoCode <35817798+SegoCode@users.noreply.github.com>
2022-01-08 20:33:43 +0100

    Update login.test.js

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -9,22 +9,22 @@ beforeEach(() => {
 
 describe('Testing testing access to endpoints', () => {
 	describe('Public endpoints', () => {
-		test('GET /api/', async () => {
-			await api.get('/api/').expect(200).expect('Content-Type', /json/);;
+		test('GET /1.0/', async () => {
+			await api.get('/1.0/').expect(200).expect('Content-Type', /json/);;
 		});
 
-		test('POST /api/auth/login/', async () => {
+		test('POST /1.0/auth/login/', async () => {
 			let body = {
 				user: 'test',
 				pass: 'test',
 			};
-			await api.post('/api/auth/login/').send(body).expect(200).expect('Content-Type', /json/);;
+			await api.post('/1.0/auth/login/').send(body).expect(200).expect('Content-Type', /json/);;
 		});
 	});
 
 	describe('Private endpoints', () => {
-		test('GET /api/auth/login/refresh/', async () => {
-			await api.get('/api/auth/login/refresh').expect(401).expect('Content-Type', /json/);;
+		test('GET /1.0/auth/login/refresh/', async () => {
+			await api.get('/1.0/auth/login/refresh').expect(401).expect('Content-Type', /json/);;
 		});
 	});
 });
