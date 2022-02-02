460b3a5ad770fac5ff9e72ded5fb6b97d3343268
SegoCode <35817798+SegoCode@users.noreply.github.com>
2022-02-02 22:26:48 +0100

    Create swagger endpoint

--- src/routes/root.router.js
+++ src/routes/root.router.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../documents/swagger.json');
+
 const messages = require('../utils/message.utils');
 const reqAuth = require('../middlewares/auth.handler');
 
-//GET
 router.get('/', reqAuth.authRole(reqAuth.ROLE.PUBLIC), function (req, res) {
 	//TODO: Generate swagger documentation
-	res.status(messages.OK_CODE).json(messages.RESPOND_OK_CUSTOM('Consult API documentation:'));
+	res.status(messages.OK_CODE).json(messages.RESPOND_OK_CUSTOM('Consult API documentation: /api-docs'));
 });
+
+
+
+var options = {
+	customCss: '.swagger-ui .topbar { display: none }',
+	customSiteTitle: "API REST FULL",
+	customfavIcon: "/assets/favicon.ico"
+  };
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument,options));
+
+
 
 module.exports = router;
